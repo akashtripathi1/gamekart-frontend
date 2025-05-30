7c11fdb4c08f8c0bff3c127639185601c7377bff
Akash Tripathi <akashtripathi1729@gmail.com>
2025-05-30 14:26:11 +0530

    adding logs to check error

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 // src/App.jsx
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { setAuthState } from '@/store/slice/authSlice';
-import { RouterProvider } from 'react-router-dom';
-import router from './router/Router';
-import { Toaster } from 'react-hot-toast';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setAuthState } from "@/store/slice/authSlice";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/Router";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -14,9 +14,16 @@ const App = () => {
       try {
         const response = await fetch(
           `${import.meta.env.VITE_BACKEND_URL}/api/auth/current_user`,
-          { credentials: 'include' }
+          { credentials: "include" }
         );
+
+        console.log("Fetch Response:", response);
+        console.log("Response Status:", response.status);
+
         const data = await response.json();
+
+        console.log("Session Data:", data);
+
         dispatch(
           setAuthState({
             isAuthenticated: data.isAuthenticated,
@@ -24,6 +31,8 @@ const App = () => {
           })
         );
       } catch (error) {
+        console.error("Session check failed:", error);
+
         dispatch(
           setAuthState({
             isAuthenticated: false,
@@ -32,6 +41,7 @@ const App = () => {
         );
       }
     };
+
     checkSession();
   }, [dispatch]);
 
