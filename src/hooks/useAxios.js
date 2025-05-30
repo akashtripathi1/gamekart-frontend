1906706c3460edf17a1b5f204cd52833065c3f3e
Akash Tripathi <akashtripathi1729@gmail.com>
2025-05-30 15:01:38 +0530

    fixed useAxios hook

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -10,9 +10,7 @@ export const makeRequest = axios.create({
 makeRequest.interceptors.request.use((config) => {
   const state = store.getState();
   const token = state.auth.token;
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+
   return config;
 });
 
